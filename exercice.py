5fd527cb4fe8a869f41fe00939e2268c5914b42a
LaureneRly <laurene.ralay@polymtl.ca>
2020-09-14 22:49:20 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -13,8 +13,8 @@ def square(a: float) -> float:
 
 
 def average(a: float, b: float, c: float) -> float:
-    averag = (a+b+c)/3
-    return math. 
+    avrg = (a+b+c)/3
+    return avrg 
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
