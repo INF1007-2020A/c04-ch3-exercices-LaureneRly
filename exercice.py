574a2ed739b1308a7a25dae4c7cfa37280a8d00d
LaureneRly <laurene.ralay@polymtl.ca>
2020-09-14 22:48:00 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -18,7 +18,7 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    deci = angle_
+    deci = angle_degs+()
     return 0.0
 
 
