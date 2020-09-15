3baa588c9d0ae3f8d26caa2710305cb7153947e9
LaureneRly <laurene.ralay@polymtl.ca>
2020-09-15 03:42:10 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -36,7 +36,7 @@ def to_celsius(temperature: float) -> float:
 
 
 def to_farenheit(temperatur: float) -> float:
-    return (temperatur*(9/5))+32
+    return (temperatur*(1.))+32
 
 
 def main() -> None:
